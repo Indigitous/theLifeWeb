20b49a56ee51006bcacb727f155ba6a0ca8e032a
Vasily Polovnyov <vazilla@gmail.com>
2013-04-02 13:38:55 +0400

    add corresponding api_taster route

--- app/api_tasters/v1/groups.rb
+++ app/api_tasters/v1/groups.rb
@@ -6,4 +6,12 @@ ApiTaster::RouteCollector.route do
   post '/v1/groups',
     FactoryGirl.attributes_for(:group)
       .merge(authentication_token: Examples.user.authentication_token)
+
+  desc <<-markdown.strip_heredoc
+    ## Get all groups in the app
+  markdown
+
+  get '/v1/groups',
+    authentication_token: Examples.user.authentication_token
+
 end
