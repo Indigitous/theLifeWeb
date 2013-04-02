55e9515aeee69e4162affbe0530b678f94fefb4e
Vasily Polovnyov <vazilla@gmail.com>
2013-04-02 19:15:18 +0400

    do not forget about general API specs

--- spec/api/v1/groups_spec.rb
+++ spec/api/v1/groups_spec.rb
@@ -5,11 +5,12 @@ describe 'v1/groups' do
   let(:authentication_token) { current_user.authentication_token }
   let(:params) { FactoryGirl.attributes_for(:group) }
 
-  before do
-    post 'v1/groups', params.merge(authentication_token: authentication_token)
-  end
 
   describe 'creating a group' do
+    before do
+      post 'v1/groups', params.merge(authentication_token: authentication_token)
+    end
+
     subject { json_response_body }
     it { should be_a_group_representation(current_user.owned_groups.first) }
 
@@ -25,5 +26,18 @@ describe 'v1/groups' do
 
       it { should have_error("can't be blank").on('description') }
     end
+  end
+
+  describe 'list groups' do
+    let!(:group) { FactoryGirl.create(:group) }
+
+    before do
+      get 'v1/groups.json', authentication_token: authentication_token
+    end
+
+    subject { json_response_body }
+
+    it { should be_a_kind_of Array }
+    its(:first) { should be_a_group_representation(group) }
   end
 end
