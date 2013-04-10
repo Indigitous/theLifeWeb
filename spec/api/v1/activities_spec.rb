07579f19bf95c89e93f1e5dec77009d9f4e5bf09
Timur Khafizov <timur.khafizov@flatstack.com>
2013-04-10 12:18:44 +0400

    Fix general activities spec to create two different thresholds

--- spec/api/v1/activities_spec.rb
+++ spec/api/v1/activities_spec.rb
@@ -5,8 +5,8 @@ describe '/v1/activities' do
   let(:authentication_token) { current_user.authentication_token }
 
   describe 'show activities apllicable for friend' do
-    let(:threshold) { create :threshold }
-    let(:another_threshold) { create :threshold }
+    let(:threshold) { create :threshold, title: Threshold::TITLES[0] }
+    let(:another_threshold) { create :threshold, title: Threshold::TITLES[1] }
 
     before do
       create(:activity, thresholds: [threshold])
