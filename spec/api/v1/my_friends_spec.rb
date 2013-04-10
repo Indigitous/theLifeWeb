292de8f64058a5f6cb607ed47994e794f14e0036
Timur Khafizov <timur.khafizov@flatstack.com>
2013-04-10 15:43:01 +0400

    Fix general test for friends

--- spec/api/v1/my_friends_spec.rb
+++ spec/api/v1/my_friends_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
 
-describe '/v1/my_events' do
+describe '/v1/my_friends' do
   let(:current_user) { create :user }
   let(:authentication_token) { current_user.authentication_token }
+  let!(:friend) { create :friend, user: current_user }
 
   describe 'show all friends of this user' do
     before do
-      create :friend, user: current_user
       get 'v1/my_friends', authentication_token: authentication_token
     end
 
     subject { json_response_body }
 
-    it { should be_a_friend_representation(current_user.friends.first) }
+    it { should be_a_kind_of Array }
+    it { should be_a_friend_representation(friend) }
   end
 end
