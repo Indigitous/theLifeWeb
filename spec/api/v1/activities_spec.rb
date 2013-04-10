11776d69a3cb77ec4541796c890929bbdd6ab3ee
Timur Khafizov <timur.khafizov@flatstack.com>
2013-04-10 16:47:22 +0400

    FIx activities test styling

--- spec/api/v1/activities_spec.rb
+++ spec/api/v1/activities_spec.rb
@@ -7,9 +7,9 @@ describe '/v1/activities' do
   describe 'show activities apllicable for friend' do
     let(:threshold) { create :threshold, title: Threshold::TITLES[0] }
     let(:another_threshold) { create :threshold, title: Threshold::TITLES[1] }
+    let!(:activity) { create :activity, thresholds: [threshold] }
 
     before do
-      create(:activity, thresholds: [threshold])
       create(:activity, thresholds: [another_threshold])
 
       get 'v1/activities',
@@ -19,11 +19,7 @@ describe '/v1/activities' do
 
     subject { json_response_body }
 
-    it do
-      should be_an_activity_representation(
-        Threshold.find(threshold.id).activities.first
-      )
-    end
+    it { should be_an_activity_representation(activity) }
     its(:size) { should eq(1) }
   end
 end
