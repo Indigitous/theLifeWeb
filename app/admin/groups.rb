c8ea335c821eb32626cee69975a054e02cc7d542
Adrian Teh <adrian@codespore.com>
2013-04-11 16:55:35 -0400

    AT-46246789-Able-to-see-and-edit-users-in-group-view

--- app/admin/groups.rb
+++ app/admin/groups.rb
@@ -9,6 +9,30 @@ ActiveAdmin.register Group do
     default_actions
   end
 
+  show do
+    attributes_table *default_attribute_table_rows
+    panel "Users" do
+      paginated_collection(resource.users.page(params[:page]).per(25), download_links: false) do
+        table_for(collection, sortable: false) do
+          column :email
+          column :first_name
+          column :last_name
+          column '' do |user|
+            links = ''.html_safe
+            if controller.action_methods.include?('show')
+              links << link_to(I18n.t('active_admin.view'), admin_user_path(user), :class => "member_link view_link")
+            end
+            if controller.action_methods.include?('edit')
+              links << link_to(I18n.t('active_admin.edit'), edit_admin_user_path(user), :class => "member_link edit_link")
+            end
+            links
+          end
+        end
+      end
+    end
+    active_admin_comments
+  end
+
   controller do
     def scoped_collection
       Group.includes(:owner)
