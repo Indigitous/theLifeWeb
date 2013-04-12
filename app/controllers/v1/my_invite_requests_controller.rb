2a039be7cfdee0ef34ab0645a65d1fae8fc53243
Vasily Polovnyov <vazilla@gmail.com>
2013-04-12 13:47:45 +0400

    add description to field to /my_requests response JSON

--- app/controllers/v1/my_invite_requests_controller.rb
+++ app/controllers/v1/my_invite_requests_controller.rb
@@ -2,6 +2,6 @@ class V1::MyInviteRequestsController < ApplicationController
   expose(:my_invite_requests) { InviteRequestGatheringService.new(current_user).gather }
 
   def index
-    respond_with(my_invite_requests)
+    respond_with(my_invite_requests, each_serializer: MyInviteRequestSerializer)
   end
 end
---
+++ spec/support/matchers/be_a_my_invite_request_representation.rb
@@ -0,0 +1,19 @@
+RSpec::Matchers.define :be_a_my_invite_request_representation do |invite_request|
+  match do |json|
+    attributes = invite_request.sliced_attributes %w[
+      id
+      user_id
+      group_id
+      type
+    ]
+
+    accessors = invite_request.sliced_accessors %w[
+      description
+    ]
+
+    response_attributes = attributes.merge(accessors)
+
+    json.should be
+    json.should include_attributes(response_attributes)
+  end
+end
