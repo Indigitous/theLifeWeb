3df04dc46d86ed3ad22a9e855932c90628124ccb
Vasily Polovnyov <vazilla@gmail.com>
2013-04-19 18:47:47 +0400

    add index on settings.key

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130417093545) do
+ActiveRecord::Schema.define(:version => 20130419144652) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -137,6 +137,8 @@ ActiveRecord::Schema.define(:version => 20130417093545) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
+
+  add_index "settings", ["key"], :name => "index_settings_on_key", :unique => true
 
   create_table "thresholds", :force => true do |t|
     t.string   "title",      :default => "", :null => false
