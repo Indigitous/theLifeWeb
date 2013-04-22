0f5ca47c348551ede499fdb95a34ef01d95fa9ac
VladimirMikhailov <vladimir.mikhailov@flatstack.com>
2013-04-22 16:23:55 +0400

    Fix zeus test env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ ENV["RAILS_ENV"] ||= 'test'
 
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 
 paths = Dir[Rails.root.join('spec/**/{support,extensions}/**/*.rb')]
 paths.each { |file| require file }
