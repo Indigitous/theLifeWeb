ebefec2705bc10cf14c2d65838352bf18c604b09
Timur Khafizov <timur.khafizov@flatstack.com>
2013-04-22 16:10:18 +0400

    Temporarily revert back routes for uploading images, update ImagesController to set content type and disposition for responding image

--- app/controllers/v1/images_controller.rb
+++ app/controllers/v1/images_controller.rb
@@ -2,7 +2,7 @@ class V1::ImagesController < V1::BaseController
   def show
     image = ImagePathGettingService.new(current_user, params).get
     if image
-      send_file image
+      send_file image, type: 'image/jpeg', disposition: 'attachment'
     else
       head :not_found
     end
