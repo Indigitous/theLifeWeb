9a1eecdfc3292d7167532edad8e974dbad2d4f04
Vasily Polovnyov <vazilla@gmail.com>
2013-04-22 19:37:21 +0400

    fix my_events serialization

--- app/controllers/v1/my_events_controller.rb
+++ app/controllers/v1/my_events_controller.rb
@@ -2,6 +2,6 @@ class V1::MyEventsController < V1::BaseController
   expose(:my_events) { EventGatheringService.new(current_user).gather(params) }
 
   def index
-    respond_with(my_events)
+    respond_with(my_events, each_serializer: MyEventSerializer)
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -10,7 +10,11 @@ class Event < ActiveRecord::Base
     presence: true
 
   delegate :summary, to: :activity
+  delegate :first_name, to: :user, prefix: true
+  delegate :first_name, to: :friend, prefix: true
 
+  alias_method :user_name, :user_first_name
+  alias_method :friend_name, :friend_first_name
   alias_method :description, :summary
 
   scope :recent, lambda { |n| limit(n).order('id desc') }
