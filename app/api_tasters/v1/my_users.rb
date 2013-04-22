077f3eacccb181e6e02928c078a190c918b03870
Timur Khafizov <timur.khafizov@flatstack.com>
2013-04-22 20:05:40 +0400

    Remove Api Taster route for updating user image, update version detection for ImagePathGettingService

--- app/api_tasters/v1/my_users.rb
+++ app/api_tasters/v1/my_users.rb
@@ -9,15 +9,4 @@ ApiTaster::RouteCollector.route do
   get '/v1/my_users/:id',
     id: Examples.user.id,
     authentication_token: Examples.user.authentication_token
-
-  desc <<-markdown.strip_heredoc
-    ## Update user's image
-
-    `:id` must be current user
-  markdown
-
-  post '/v1/my_users/:id/image',
-    id: Examples.user.id,
-    image: :file,
-    authentication_token: Examples.user.authentication_token
 end
