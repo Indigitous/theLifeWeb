c2ca56d5e89f7114024459af9d36b85d78432ab9
Timur Khafizov <timur.khafizov@flatstack.com>
2013-04-22 19:53:54 +0400

    Fix BaseSerializer to not respond with unexisting image URL

--- app/serializers/base_serializer.rb
+++ app/serializers/base_serializer.rb
@@ -4,10 +4,10 @@ class BaseSerializer < ActiveModel::Serializer
   def image_attributes
     img_attributes = {}
 
-    image_url = "v1/image/#{object.class.to_s.downcase.pluralize}/#{object.id}"
-    img_attributes['image'] = image_url
-
     unless object.image.blank?
+      image_url = "v1/image/#{object.class.to_s.downcase.pluralize}/#{object.id}"
+      img_attributes['image'] = image_url
+
       object.image.versions.keys.each do |version|
         img_attributes[version] = image_url + "/#{version.to_s}"
       end
