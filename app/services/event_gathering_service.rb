b56f4b555dfa5715a662891acff2e2e046e3f9af
Vasily Polovnyov <vazilla@gmail.com>
2013-04-22 19:42:25 +0400

    fix N+1 warnings

--- app/services/event_gathering_service.rb
+++ app/services/event_gathering_service.rb
@@ -12,7 +12,9 @@ class EventGatheringService
   private
 
   def collect
-    Event.where(user_id: group_comembers_id.push(@user.id))
+    Event
+      .where(user_id: group_comembers_id.push(@user.id))
+      .includes(:user, :friend, :activity)
   end
 
   def group_comembers_id
