1c9af62f6610c0cbd0a3f8d5c3b51b2043078a1d
Timur Khafizov <timur.khafizov@flatstack.com>
2013-04-23 12:33:27 +0400

    Add access restriction for non-accessible resources in ImagePathGettingService, provide tests for it, fix `users#visible_user_ids` result not to contain duplicated elements

--- spec/services/image_path_getting_service_spec.rb
+++ spec/services/image_path_getting_service_spec.rb
@@ -11,20 +11,32 @@ describe ImagePathGettingService do
     context 'when requested resource is not allowed' do
       let(:params) { { resources: 'babes', id: current_user.id } }
 
-
       it { should_not be }
     end
 
-    context 'user' do
+    context "user's image" do
+      let!(:other_user) { create :user }
+      let(:user_params) { { resources: 'users', id: current_user.id } }
+
+      describe 'when user has not such comember' do
+        let(:params) { { resources: 'users', id: other_user.id } }
+
+        before do
+          User.any_instance.should_not_receive(:image_url)
+        end
+
+        it { should_not be }
+      end
+
       describe 'when user has no image attached to' do
-        let(:params) { { resources: 'users', id: current_user.id } }
+        let(:params) { user_params }
 
         it { should_not be }
       end
 
       describe 'when user has image attached to' do
         context 'user requests base version of image' do
-          let(:params) { { resources: 'users', id: current_user.id } }
+          let(:params) { user_params }
 
           before do
             User.any_instance.should_receive(:image_url)
@@ -36,17 +48,58 @@ describe ImagePathGettingService do
 
         context 'user requests non-base version of image' do
           let(:version) { "thumbnail" }
-          let(:params) do
-            {
-              resources: 'users',
-              id: current_user.id,
-              version: version
-            }
-          end
+          let(:params) { user_params.merge(version: version) }
 
           before do
             User.any_instance.should_receive(:image_url).with(version)
             User.any_instance.stub(:image_url).with(version) {'some/path/to/image'}
+          end
+
+          it { should be_a_kind_of String }
+        end
+      end
+    end
+
+    context "friend's image" do
+      let!(:friend) { create :friend, user: current_user }
+      let!(:other_friend) { create :friend }
+      let(:friend_params) { { resources: 'friends', id: friend.id } }
+
+      describe 'when user has not such friend' do
+        let(:params) { { resources: 'friends', id: other_friend.id } }
+
+        before do
+          Friend.any_instance.should_not_receive(:image_url)
+        end
+
+        it { should_not be }
+      end
+
+      describe 'when friend has not attached image' do
+        let(:params) { friend_params }
+
+        it { should_not be }
+      end
+
+      describe 'when friend has attached image' do
+        context 'user requests base version of image' do
+          let(:params) { friend_params }
+
+          before do
+            Friend.any_instance.should_receive(:image_url)
+            Friend.any_instance.stub(image_url: 'some/path/to/image')
+          end
+
+          it { should be_a_kind_of String }
+        end
+
+        context 'user requests non-base version of image' do
+          let(:version) { "thumbnail" }
+          let(:params) { friend_params.merge(version: version) }
+
+          before do
+            Friend.any_instance.should_receive(:image_url).with(version)
+            Friend.any_instance.stub(:image_url).with(version) {'some/path/to/image'}
           end
 
           it { should be_a_kind_of String }
