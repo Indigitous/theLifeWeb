f9f38064d59a6deae6482b3437de121f1983b5cd
Vasily Polovnyov <vazilla@gmail.com>
2013-04-23 16:29:04 +0400

    alias_attribute to the rescue

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,11 +10,9 @@ class Event < ActiveRecord::Base
     presence: true
 
   delegate :summary, to: :activity
-  delegate :first_name, to: :user, prefix: true
-  delegate :first_name, to: :friend, prefix: true
+  delegate :name, to: :user, prefix: true
+  delegate :name, to: :friend, prefix: true
 
-  alias_method :user_name, :user_first_name
-  alias_method :friend_name, :friend_first_name
   alias_method :description, :summary
 
   scope :recent, lambda { |n| limit(n).order('id desc') }
