8eacba974dcf5801b46ed99d2b98854f7d043ffa
Timur Khafizov <timur.khafizov@flatstack.com>
2013-04-24 17:06:13 +0400

    Categories should not return activities

--- spec/support/matchers/be_a_category_representation.rb
+++ spec/support/matchers/be_a_category_representation.rb
@@ -1,16 +1,10 @@
 RSpec::Matchers.define :be_a_category_representation do |group|
   match do |json|
-    attributes = group.sliced_attributes %w[
+    response_attributes = group.sliced_attributes %w[
       id
       name
       description
     ]
-
-    accessors = group.sliced_accessors %w[
-      activities
-    ]
-
-    response_attributes = attributes.merge(accessors)
 
     json.should be
     json.should include_attributes(response_attributes)
