c16afc8e9a7bf4df162446d17f7a09e08f13b4cf
Timur Khafizov <timur.khafizov@flatstack.com>
2013-04-25 22:07:46 +0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130423083529) do
+ActiveRecord::Schema.define(:version => 20130425151757) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -165,8 +165,8 @@ ActiveRecord::Schema.define(:version => 20130423083529) do
     t.string   "first_name",             :default => "",   :null => false
     t.string   "last_name",              :default => "",   :null => false
     t.string   "mobile"
-    t.string   "locale",                 :default => "en", :null => false
     t.string   "image"
+    t.string   "locale",                 :default => "en", :null => false
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
