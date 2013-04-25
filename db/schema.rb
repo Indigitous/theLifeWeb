f377dcaa5a546ff1f1db87c6778e7583c4070b9d
Vasily Polovnyov <vazilla@gmail.com>
2013-04-25 19:19:38 +0400

    add locale field to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130419144652) do
+ActiveRecord::Schema.define(:version => 20130425151757) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -147,8 +147,8 @@ ActiveRecord::Schema.define(:version => 20130419144652) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",   :null => false
+    t.string   "encrypted_password",     :default => "",   :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -158,11 +158,12 @@ ActiveRecord::Schema.define(:version => 20130419144652) do
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.string   "authentication_token"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "first_name",             :default => "", :null => false
-    t.string   "last_name",              :default => "", :null => false
+    t.datetime "created_at",                               :null => false
+    t.datetime "updated_at",                               :null => false
+    t.string   "first_name",             :default => "",   :null => false
+    t.string   "last_name",              :default => "",   :null => false
     t.string   "mobile"
+    t.string   "locale",                 :default => "en", :null => false
   end
 
   add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
