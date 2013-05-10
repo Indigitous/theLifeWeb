51d87f648fbe89cbaa7fc54afc386577860545ed
VladimirMikhailov <vladimir.mikhailov@flatstack.com>
2013-05-10 11:18:16 +0400

    Fix broken specs

--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe Friend do
   it { should validate_presence_of(:first_name) }
-  it { should validate_presence_of(:last_name) }
   it { should validate_presence_of(:user) }
   it { should validate_presence_of(:threshold) }
 
@@ -10,6 +9,7 @@ describe Friend do
 
   describe '#to_s' do
     let!(:friend) { create(:friend, first_name: 'Adrian', last_name: 'Teh') }
+
     subject { friend.to_s }
     it 'should return firstname and lastname' do
       subject.should == 'Adrian Teh'
