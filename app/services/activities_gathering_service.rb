0fb716ed2314d71c24cedff38ac795d7a292e64f
Timur Khafizov <timur.khafizov@flatstack.com>
2013-05-20 12:58:16 +0400

    DRY ActivitiesGatheringService

--- app/services/activities_gathering_service.rb
+++ app/services/activities_gathering_service.rb
@@ -1,10 +1,11 @@
 class ActivitiesGatheringService
   def initialize(params)
     @params = params
+    @activities = Activity.scoped
   end
 
   def gather
-    filter_activities_by_threshold_id &&
+    filter_activities_by_threshold_id
     filter_activities_by_timestamp
 
     @activities
@@ -13,18 +14,22 @@ class ActivitiesGatheringService
   private
 
   def filter_activities_by_threshold_id
-    if @params[:threshold_id].present?
-      @activities = Threshold.find(@params[:threshold_id]).activities
-    else
-      @activities = Activity.scoped
+    if threshold_id
+      @activities = Activity
+        .joins(:thresholds)
+        .where('thresholds.id' => threshold_id)
     end
   end
 
   def filter_activities_by_timestamp
     if @params[:timestamp].present?
-      @activities = @activities.where("updated_at > ?",
-        Time.at(@params[:timestamp].to_i)
+      @activities = @activities.where(
+        "updated_at > ?", Time.at(@params[:timestamp].to_i)
       )
     end
+  end
+
+  def threshold_id
+    @params[:threshold_id].presence
   end
 end
