c542cb0d1e86388c84ac2c0c369239dd59eef9d1
Timur Khafizov <timur.khafizov@flatstack.com>
2013-05-27 16:56:13 +0400

    Add new profile for SimpleCov that excludes unnecessary files from coverage report

---
+++ lib/simplecov/simplecov_rails_api_profile.rb
@@ -0,0 +1,8 @@
+require 'simplecov'
+SimpleCov.adapters.define 'rails-api' do
+  load_adapter 'rails'
+  add_filter 'api_taster'
+  add_filter 'examples'
+  add_filter 'serializers/'
+  add_group 'Admin', 'app/admin'
+end
