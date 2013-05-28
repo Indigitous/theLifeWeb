caa6d6ba02993243e999a959bb8df4d24bf33065
Timur Khafizov <timur.khafizov@flatstack.com>
2013-05-28 12:18:21 +0400

    Minor refactor for `image_attributes`

--- lib/image_attributes.rb
+++ lib/image_attributes.rb
@@ -4,16 +4,26 @@ module ImageAttributes
   end
 
   def add_image_attributes(attributes)
-    if object.image?
-      image_url = "v1/image/#{object.class.to_s.downcase.pluralize}/#{object.id}"
-    end
-
     attributes['image'] = image_url
 
     object.image.versions.keys.each do |version|
-      attributes[version] = image_url.present? ? [image_url, version].join('/') : nil
+      attributes[version] = image_url(version)
     end
 
     attributes
+  end
+
+  private
+
+  def image_url(version = nil)
+    object.image_url(version).present? ? version_image_url(version) : nil
+  end
+
+  def version_image_url(version)
+    version.nil? ? base_image_url : [base_image_url, version].join('/')
+  end
+
+  def base_image_url
+    "v1/image/#{object.class.to_s.downcase.pluralize}/#{object.id}"
   end
 end
