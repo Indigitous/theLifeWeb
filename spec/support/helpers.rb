f9f50bef030bd62e64ad6c0d0ab6ed282566145e
Timur Khafizov <timur.khafizov@flatstack.com>
2013-05-30 18:21:17 +0400

    Update hash syntax for redefined `sign_in` method for tests

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -16,11 +16,11 @@ module Helpers
   def sign_in(user = stub_model(User))
     if user.nil?
       request.env['warden'].stub(:authenticate!).
-        and_throw(:warden, {:scope => :user})
-      controller.stub :current_user => nil
+        and_throw(:warden, { scope: :user })
+      controller.stub(current_user: nil)
     else
-      request.env['warden'].stub :authenticate! => user
-      controller.stub :current_user => user
+      request.env['warden'].stub(authenticate!: user)
+      controller.stub(current_user: user)
     end
   end
 end
