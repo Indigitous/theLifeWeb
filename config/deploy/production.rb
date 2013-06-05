e1d0a2779d0517ae6de6c1cef00f932be4ddc01b
clarencemartens <clarence@ballistiq.com>
2013-06-04 21:29:14 -0700

    capistrano for production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,16 +1,15 @@
-# TODO: Set these according to the production server
-set :dns_name, "dev.ballistiq.com"
+set :dns_name, "srv1.thelifeapp.com"
 
 role :web, dns_name                          # Your HTTP server, Apache/etc
 role :app, dns_name                          # This may be the same as your `Web` server
 role :db,  dns_name, primary: true           # This is where Rails migrations will run
 
-set :deploy_to, "/home/ubuntu/webprojects/thelife"
+set :deploy_to, "/var/www/html/theLifeWeb"
 
 set :rails_env, 'production'
 set :branch, 'master'
 set :use_sudo, false
 
-set :user, 'ubuntu'
+set :user, 'clarence'
 set :port, 22
 set :db_username, 'root'
