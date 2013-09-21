e45fcbaaa5c74a498888474e53a70beaf6d3f021
clarencemartens <clarence@ballistiq.com>
2013-09-20 18:07:21 -0700

    don't allow a user to have friends with the same name

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -10,10 +10,15 @@ class Friend < ActiveRecord::Base
     :user,
     :threshold,
     presence: true
+  validates :last_name, uniqueness: { scope: [ :user_id, :first_name ] }
 
   mount_uploader :image, ImageUploader
 
   scope :recent, lambda { |n| includes(:user,:threshold).limit(n).order('id desc') }
+
+  def full_name
+    [first_name, last_name].compact.join(' ')
+  end
 
   def to_s
     "#{self.first_name} #{self.last_name}"
