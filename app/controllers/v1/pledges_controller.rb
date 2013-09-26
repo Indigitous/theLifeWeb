312035ecab2b0a59dd6e41391fd9d90506485537
clarencemartens <clarence@ballistiq.com>
2013-09-26 15:23:49 -0700

    support pledges in the GCM

--- app/controllers/v1/pledges_controller.rb
+++ app/controllers/v1/pledges_controller.rb
@@ -8,6 +8,7 @@ class V1::PledgesController < V1::BaseController
   def create
     pledge.event = event
     new_pledge = PledgeCreatingService.new(pledge).create
+    GCMService.new().send_pledge_notifications(current_user, new_pledge.event, current_user.fellow_group_members) if new_pledge
 
     respond_with(new_pledge)
   end
